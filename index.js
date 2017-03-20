4979e11d892cd6b01d732753502cfe69e5204a5c
mooran <mooran953@gmail.com>
2017-03-20 19:26:29 +0800

    Update wxc-button to use vue2.0

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-require('./src/wxc-button.we');
-require('./src/wxc-hn.we');
-require('./src/wxc-list-item.we');
-require('./src/wxc-panel.we');
-require('./src/wxc-tip.we');
-require('./src/wxc-countdown.we');
-require('./src/wxc-marquee.we');
-require('./src/wxc-navbar.we');
-require('./src/wxc-navpage.we');
-require('./src/wxc-tabbar.we');
-require('./src/wxc-tabitem.we');
+// require('./src/wxc-button.we');
+// require('./src/wxc-hn.we');
+// require('./src/wxc-list-item.we');
+// require('./src/wxc-panel.we');
+// require('./src/wxc-tip.we');
+// require('./src/wxc-countdown.we');
+// require('./src/wxc-marquee.we');
+// require('./src/wxc-navbar.we');
+// require('./src/wxc-navpage.we');
+// require('./src/wxc-tabbar.we');
+// require('./src/wxc-tabitem.we');
+
+var WxcButton = require('./src/wxc-button.we');
+
+module.exports = {
+    WxcButton: WxcButton
+}
